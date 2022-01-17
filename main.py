ad6d3b6c40367c6c28cd26aa1af2e963d87fd9fe
KitKat420 <danteclark66@gmail.com>
2022-01-17 06:58:15 -0500

    part of win condition works

--- main.py
+++ main.py
@@ -8,9 +8,24 @@ positions = [[' ', ' ', ' '],
              ]
 
 
-def board(row, column, player):
+def is_game_over():
+    """This function checks to see if a player won"""
+    global is_game_on
+    current_row = 0
 
+    while current_row <= 2:
+        if positions[current_row][0] == "X" and positions[current_row][1] == "X" and positions[current_row][2] == "X":
+            is_game_on = False
+        elif positions[current_row][0] == "O" and positions[current_row][1] == "O" and positions[current_row][2] == "O":
+            is_game_on = False
+        current_row += 1
+
+
+def board(row, column, player):
+    """This function maintains the current state of game board"""
     positions[row][column] = player
+
+    is_game_over()
 
     pos = [[f"\t   {positions[0][0]}  |", f" {positions[0][1]} ", f"| {positions[0][2]}"],
            [f"\t   {positions[1][0]}  |",
@@ -28,6 +43,7 @@ def board(row, column, player):
 
 
 def player_turn():
+    """This function changes turns"""
     global turn
     global player_shape
     if turn:
@@ -38,11 +54,12 @@ def player_turn():
         turn = True
 
 
-is_game_on = True
-
 player_shape = input("Player #1 choose shape: ")
 
+is_game_on = True
+
 while is_game_on:
+    is_game_over()
     row = int(input("Choose row: "))
     column = int(input("Choose column: "))
     board(row, column, player_shape)
