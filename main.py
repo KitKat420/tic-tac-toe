e2f2ecfb17d82ada719efe7853b9552a0fc28a91
KitKat420 <danteclark66@gmail.com>
2022-01-17 06:09:07 -0500

    player turns added & shapes added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from operator import indexOf
 
+turn = True
 
 positions = [[' ', ' ', ' '],
              [' ', ' ', ' '],
@@ -7,9 +8,9 @@ positions = [[' ', ' ', ' '],
              ]
 
 
-def board(row, column):
+def board(row, column, player):
 
-    positions[row][column] = "X"
+    positions[row][column] = player
 
     pos = [[f"\t   {positions[0][0]}  |", f" {positions[0][1]} ", f"| {positions[0][2]}"],
            [f"\t   {positions[1][0]}  |",
@@ -26,9 +27,23 @@ def board(row, column):
     print(f"\t      |     |   ")
 
 
+def player_turn():
+    global turn
+    global player_shape
+    if turn:
+        player_shape = "O"
+        turn = False
+    else:
+        player_shape = "X"
+        turn = True
+
+
 is_game_on = True
+
+player_shape = input("Player #1 choose shape: ")
 
 while is_game_on:
     row = int(input("Choose row: "))
     column = int(input("Choose column: "))
-    board(row, column)
+    board(row, column, player_shape)
+    player_turn()
